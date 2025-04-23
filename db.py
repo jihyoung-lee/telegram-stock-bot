c71f16a7a5473962612b08c054a9636843f5d6f6
지형 <jihyoung.lee90@gmail.com>
2025-04-23 14:28:55 +0900

    주석제거

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 import logging
-DATABASE_URL = os.environ.get("DATABASE_URL")  # Render의 Internal URL 사용
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_connection():
     try:
